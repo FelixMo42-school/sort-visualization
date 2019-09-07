db1d591ff0abba6f6343afc864b19a57e9774815
FelixMo42 <felix@tacocat.com>
2019-09-07 10:35:37 -0700

    astar now works + updated heap

--- astar.js
+++ astar.js
@@ -1,14 +1,13 @@
 const params = (new URL(document.location)).searchParams
 const num =  20
 const delay = 100
-const data = []
 
 let offset = 3
 let start = [offset, offset]
 let end = [num - offset - 1, num - offset - 1]
 
-Array.prototype.loc = function(x, y) {
-    return this[0] == x && this[1] == y
+Array.prototype.loc = function(pos) {
+    return this[0] == pos[0] && this[1] == pos[1]
 }
 
 Array.prototype.dist = function(target) {
@@ -16,30 +15,12 @@ Array.prototype.dist = function(target) {
 }
 
 class Node {
-    constructor({pos, parent=false}) {
-        this.pos = pos
-        this.x = pos[0]
-        this.y = pos[1]
-
-        this.dist = pos.dist(end)
-        this.cost = this.dist
-        if (parent) {
-            this.cost += parent.cost + pos.dist(parent.pos)
-        }
-
-        this.parent = parent
-
-        this.priority = this.dist + this.cost
-    }
-}
-
-class Node2 {
-    constructor({position}) {
+    constructor({position, walkable}) {
         this.position = position
         this.x = position[0]
         this.y = position[1]
 
-
+        this.walkable = walkable
     }
 }
 
@@ -54,7 +35,8 @@ class Map {
             this.nodes.push([])
             for (let y = 0; y < this.height; y++) {
                 this.nodes[x].push(new Node({
-                    position: [x, y]
+                    position: [x, y],
+                    walkable: Math.random() > .3
                 }))
             }
         }
@@ -65,86 +47,55 @@ class Map {
     }
 }
 
-let map = new Map({num, num})
+let map = new Map({width: num, height: num})
+map.get(start).walkable = true
+map.get(start).onPath = true
+map.get(end).walkable = true
+map.get(end).onPath = true
 
-function astarV1() {
+function astar() {
     let open = new Heap()
-
-    function close(node) {
-        data[node.x][node.y] = -2
-    }
 
     function colorPath(node) {
-        clearInterval(sortLoop)
-
-        data[node.x][node.y] = 2
-        while (node.parent) {
-            node = node.parent
-            data[node.x][node.y] = 2
+        while ("previous" in node) {
+            node.onPath = true
+            node = node.previous
         }
     }
-
-    open.add(new Node({
-        pos: start
-    }))
-
-    return () => {
-        let current = open.pull()
-
-        if (!current) {
-            return clearInterval(sortLoop)
-        }
-
-        close(current)
-
-        for (let x = current.x - 1; x <= current.x + 1; x++) {
-            for (let y = current.y - 1; y <= current.y + 1; y++) {
-                if (
-                    x >= 0 && y >= 0 &&
-                    x < num && y < num &&
-                    !(x == 0 && y == 0) &&
-                    data[x][y] >= 0
-                ) {
-                    if (end.loc(x, y)) {
-                        return colorPath(current)
-                    }
-
-                    if (data[x][y] == 1) {
-                        
-                    }
-
-                    open.add(new Node({pos: [x, y], parent: current}))
-                    data[x][y] = 1
-                }
-            }
-        }
-    }
-}
-
-function astarV2() {
-    let open = new Heap()
 
     function add(node, previous) {
+        let cost = previous.cost + node.position.dist(previous.position)
+
+        if (node.open && cost > node.cost) {
+            return
+        }
+
         node.previous = previous
-        node.cost = previous.cost + pos.dist(previous.pos)
+        node.cost = previous.cost + node.position.dist(previous.position)
+        node.dist = node.position.dist(end)
+        node.priority = node.cost + node.dist
 
         if (node.open) {
             open.update(node)
         } else {
             node.open = true
-            node.dist = node.position.dist(end)
             open.add(node)
         }
-
-        node.priority = node.cost + node.dist
     }
 
+    map.get(start).cost = 0
+    map.get(start).priority = 0
     open.add( map.get(start) )
 
     return () => {
-        let current = open.pull()
+        let current = open.pop()
 
         if (!current) {
+            return clearInterval(sortLoop)
+        }
+
+        if (current.position.loc(end)) {
+            colorPath(current)
             return clearInterval(sortLoop)
         }
 
@@ -154,51 +105,56 @@ function astarV2() {
             for (let y = current.y - 1; y <= current.y + 1; y++) {
                 if (
                     x >= 0 && y >= 0 &&
-                    x < num && y < num &&
-                    !(x == 0 && y == 0) &&
-                    !map.get([x, y]).closed
+                    x < map.width && y < map.height &&
+                    !map.get([x, y]).closed &&
+                    map.get([x, y]).walkable
                 ) {
-                    add(map.get([x, y]))
+                    add(map.get([x, y]), current)
                 }
             }
         }
     }
 }
 
-sortLoop = setInterval(astarV1(), delay)
+sortLoop = setInterval(astar(), delay)
 
 function setup() {
     createCanvas(innerWidth, innerHeight)
-
-    for (let y = 0; y < num; y++) {
-        data.push([])
-        for (let x = 0; x < num; x++) {
-            data[y].push(0)
-        }
-    }
 }
 
 function draw() {
     let size = Math.min(innerHeight, innerWidth) / num
+
     clear()
 
-    for (let x = 0; x < num; x++) {
-        for (let y = 0; y < num; y++) {
+    for (let x = 0; x < map.width; x++) {
+        for (let y = 0; y < map.height; y++) {
+            let node = map.get([x, y])
             fill("grey")
 
-            switch (data[x][y]) {
-                case 2:
-                    fill("pink"); break
-                case -2:
-                    fill("green"); break
-                case -1:
-                    fill("black"); break
-                case 1:
-                    fill("blue"); break
-                default:
-                    fill("grey")
+            if (node.open) {
+                fill("blue")
             }
+
+            if (node.closed) {
+                fill("green")
+            }
+
+            if (node.onPath) {
+                fill("pink")
+            }
+
+            if (!node.walkable) {
+                fill("black")
+            }
+            
             rect(x * size, y * size, size, size)
+
+            if ("priority" in node) {
+                fill("black")
+                textAlign(CENTER, CENTER)
+                text( Math.floor(node.priority * 100), x * size + size / 2, y * size + size / 2 )
+            }
         }
     }
 }
